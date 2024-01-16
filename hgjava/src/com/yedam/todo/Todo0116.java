3dc511dff04bfa5f774a04a35bb10083e8da1d6b
ag707 <ag707@Agnes-J>
2024-01-16 23:02:36 +0900

    과제0116.

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 
 public class Todo0116 {
 	public static void main(String[] args) {
+		
 		Scanner scn = new Scanner(System.in);
 		
-		System.out.println("값을 입력>>> ");
-		String input = scn.nextLine();
+		while(true) {
+			System.out.println("값을 입력>>> ");
+			String input = scn.nextLine();
+			if(input.equals("quit")) {
+				System.out.println("<<end of prog>>");
+				scn.close();
+				break;
+			}else {
+				System.out.println("입력값은 " + input +" 입니다.");				
+			}
+		}
 		
 		// 입력값은 ~~ 입니다.
 		// 입력값은 ~~ 입니다.
@@ -15,5 +25,6 @@ public class Todo0116 {
 		// quit 값이 들어오면 콘솔에 <<end of prog>> 프로그램 종료시키기
 		// javascript에서는 ==를 사용해서 비교하겠지만
 		// java에서는 equals라는 메소드(?) 함수(?) 사용
-	}
-}
+		
+	}//end main
+}//end class
